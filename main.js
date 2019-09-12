b5ebdfd5aaf53f8dc61d78c3ed4478940ba5c301
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-12 11:29:07 -0400

    calling getEmployeeInformation()

--- main.js
+++ main.js
@@ -15,3 +15,7 @@ function getEmployeeInformation(inputSalary) {
     console.log('Bonus: ' + calculateBonus());
     console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
 }
+
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
