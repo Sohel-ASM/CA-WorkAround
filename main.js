0ee8f9ffe2724d3d69afeac140131e7b07201918
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-12 11:27:52 -0400

    getEmployeeInformation()

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ import {
     reimbursementEligibility
 } from './employee';
 import Employee from './employee';
+
+function getEmployeeInformation(inputSalary) {
+    Employee.salary = inputSalary;
+    console.log('Cadre: ' + getCadre());
+    console.log('Tax: ' + calculateTax());
+    console.log('Benefits: ' + getBenefits());
+    console.log('Bonus: ' + calculateBonus());
+    console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
+}
